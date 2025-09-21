630c31778dbd2ce78fa934f6129832aebc8455fd
Chad Hartman <cmhartman@gmail.com>
2025-09-21 11:18:10 -0700

    wip

---
+++ test/arena.c
@@ -0,0 +1,86 @@
+#include <assert.h>
+#include <string.h> // memset
+
+#include "arena.h"
+
+struct arena_t {
+  uint8_t *bytes;
+  size_t size;
+  size_t offset;
+
+  struct arena_t *upstream;
+};
+
+arena_t *arena_create() {
+  arena_t *restrict arena = calloc(1, sizeof(arena_t));
+  assert(arena);
+  return arena;
+}
+
+void *arena_alloc(arena_t *restrict arena, size_t bytes) {
+
+  assert(arena);
+  assert(bytes > 0);
+
+  if (arena->offset + bytes <= arena->size) {
+    void *restrict ptr = arena->bytes + arena->offset;
+    arena->offset += bytes;
+    memset(ptr, 0, bytes);
+    return ptr;
+  }
+
+  if (arena->upstream) {
+    return arena_alloc(arena->upstream, bytes);
+  }
+
+  const size_t upstream_size = arena->size == 0 ? 1024 : arena->size * 2;
+  arena->upstream = arena_create();
+  *arena->upstream = (arena_t){
+      .bytes = calloc(upstream_size, sizeof(uint8_t)),
+      .size = upstream_size,
+  };
+  assert(arena->upstream->bytes);
+  return arena_alloc(arena->upstream, bytes);
+}
+
+void *arena_copy(arena_t *restrict arena, const void *restrict src, size_t bytes) {
+
+  if (src == NULL || bytes == 0) {
+    return NULL;
+  }
+
+  assert(arena);
+  void *restrict copy = arena_alloc(arena, bytes);
+  memcpy(copy, src, bytes);
+  return copy;
+}
+
+void *arena_strdup(arena_t *restrict arena, const char *restrict src) {
+
+  if (src == NULL) {
+    return NULL;
+  }
+
+  const size_t bytes = strlen(src) + 1;
+  return arena_copy(arena, src, bytes);
+}
+
+void arena_reset(arena_t *restrict arena) {
+
+  if (!arena) {
+    return;
+  }
+
+  arena->offset = 0;
+  arena_reset(arena->upstream);
+}
+
+void arena_free(arena_t *restrict arena) {
+
+  if (!arena) {
+    return;
+  }
+
+  arena_free(arena->upstream);
+  free(arena->bytes);
+}
---
+++ test/arena.h
@@ -0,0 +1,21 @@
+#ifndef ARENA_H
+#define ARENA_H
+
+#include <stdint.h> // uint8_t
+#include <stdlib.h> // size_t
+
+typedef struct arena_t arena_t;
+
+arena_t *arena_create();
+
+void *arena_alloc(arena_t *restrict arena, size_t bytes);
+
+void *arena_copy(arena_t *restrict arena, const void *restrict src, size_t bytes);
+
+void *arena_strdup(arena_t *restrict arena, const char *restrict src);
+
+void arena_reset(arena_t *restrict arena);
+
+void arena_free(arena_t *restrict arena);
+
+#endif
