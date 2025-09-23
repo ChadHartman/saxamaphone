216ca8e2a0f62b8797ee8297141ffb7ef44d9f2f
Chad Hartman <cmhartman@gmail.com>
2025-09-23 15:55:18 -0700

    wip

--- test/main-obj-map.c
+++ test/main-obj-map.c
@@ -5,6 +5,7 @@
 #include <stdlib.h> // EXIT_SUCCESS
 #include <string.h> // strrchr
 
+#include "arena.h"
 #include "test.h"
 #include <saxamaphone.h>
 
@@ -62,6 +63,7 @@ static int32_t sax_attr_d32(sax_parser_t *restrict parser, const char *restrict 
 }
 
 static char *map_string_node(
+    arena_t *restrict arena,
     sax_parser_t *restrict parser,
     const char *restrict tag) {
 
