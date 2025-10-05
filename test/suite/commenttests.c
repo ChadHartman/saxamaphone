07cda1ac350fa1035b1b6d00fabb345cdfeb047e
chadhartman <cmhartman@gmail.com>
2025-10-05 15:17:23 -0700

    wip

--- test/suite/commenttests.c
+++ test/suite/commenttests.c
@@ -13,7 +13,20 @@ TEST(comment) {
     sax_parser_free(parser);
   }
 
-  ASSERT_XML_ERR(arena, "<!?", "Unexpected character '?' located on line 1 column 3");
+  // ASSERT_XML_ERR(arena, "<!?", "Unexpected character '?' located on line 1 column 3");
+    {                                                             \
+    arena_reset(arena);                                         \
+    sax_parser_t *restrict parser = sax_parser(&(sax_config_t){ \
+        .xml = "<!?",                                          \
+        .alloc = arena_custom_alloc,                            \
+        .alloc_ctx = arena,                                     \
+    });                                                         \
+    ASSERT_EQ(SAX_EVENT_ERROR, sax_next(parser));               \
+    if ("Unexpected character '?' located on line 1 column 3" != NULL) {                                     \
+      ASSERT_STR_EQ("Unexpected character '?' located on line 1 column 3", sax_error(parser));               \
+    }                                                           \
+    sax_parser_free(parser);                                    \
+  }
   ASSERT_XML_ERR(arena, "<!/", "Unexpected character '/' located on line 1 column 3");
   ASSERT_XML_ERR(arena, "<!!", "Unexpected character '!' located on line 1 column 3");
   ASSERT_XML_ERR(arena, "<!-foo", "Unexpected character 'f' located on line 1 column 4");
