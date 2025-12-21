d444a480acbbf587b1cee200b0868f88bd6d5fe0
Chad Hartman <cmhartman@gmail.com>
2025-12-21 09:40:50 -0800

    wip

--- test/suite/mapmalformedtests.c
+++ test/suite/mapmalformedtests.c
@@ -1,6 +1,29 @@
 #include <saxmapper.h>
 #include <test.h>
 
+static void test_mapper_no_decoder(arena_t *restrict arena) {
+
+  const sax_field_t foo_schema[] = {{.name = "bar", .type = UINT8_MAX - 1}, {0}};
+  const sax_field_t doc_schema[] = {{.name = "foo", .schema = foo_schema}, {0}};
+  typedef struct foo_t {
+    uint8_t bar;
+  } foo_t;
+
+  sax_parser_t *restrict parser = sax_parser(&(sax_config_t){
+      .alloc = arena_custom_alloc,
+      .alloc_ctx = arena,
+      .xml = "<foo bar=\"42\"/>",
+  });
+
+  foo_t foo = {0};
+  char *err = NULL;
+  ASSERT_FALSE(sax_decode(parser, doc_schema, &foo, NULL, &err));
+  ASSERT_STR_EQ("No decoder set for attribute \"bar\" with value \"42\"", err);
+  arena_custom_alloc(arena, err, 0);
+
+  sax_parser_free(parser);
+}
+
 static void test_mapper_set_error(arena_t *restrict arena) {
 
   sax_parser_t *restrict parser = sax_parser(&(sax_config_t){
@@ -20,5 +43,6 @@ static void test_mapper_set_error(arena_t *restrict arena) {
 }
 
 TEST(map_malformed) {
+  test_mapper_no_decoder(arena);
   test_mapper_set_error(arena);
 }
