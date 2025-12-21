68ad009aba591c742dff580292da5b0a82c47375
Chad Hartman <cmhartman@gmail.com>
2025-12-21 09:48:07 -0800

    wip

--- test/suite/mapmalformedtests.c
+++ test/suite/mapmalformedtests.c
@@ -7,7 +7,7 @@ static bool false_decoder(const sax_decode_ctx_t *restrict ctx, void *value) {
   return false;
 }
 
-static void test_mapper_decode_returns_false(arena_t *restrict arena) {
+static void test_mapper_decode_attr_returns_false(arena_t *restrict arena) {
 
   const sax_field_t foo_schema[] = {{.name = "bar", .type = SAX_TYPE_UINT8}, {0}};
   const sax_field_t doc_schema[] = {{.name = "foo", .schema = foo_schema}, {0}};
@@ -32,6 +32,36 @@ static void test_mapper_decode_returns_false(arena_t *restrict arena) {
       },
       &err));
   ASSERT_STR_EQ("Failed to set \"bar\" with \"42\"; decoder returned false", err);
+  arena_custom_alloc(arena, err, 0);
+
+  sax_parser_free(parser);
+}
+
+static void test_mapper_decode_content_returns_false(arena_t *restrict arena) {
+
+  typedef struct foo_t {
+    uint8_t bar;
+  } foo_t;
+  const sax_field_t foo_schema[] = {{.name = SAX_CONTENT, .type = SAX_TYPE_UINT8, .offset = offsetof(foo_t, bar)}, {0}};
+  const sax_field_t doc_schema[] = {{.name = "foo", .schema = foo_schema}, {0}};
+
+  sax_parser_t *restrict parser = sax_parser(&(sax_config_t){
+      .alloc = arena_custom_alloc,
+      .alloc_ctx = arena,
+      .xml = "<foo>42</foo>",
+  });
+
+  foo_t foo = {0};
+  char *err = NULL;
+  ASSERT_FALSE(sax_decode(
+      parser,
+      doc_schema,
+      &foo,
+      &(sax_map_opts_t){
+          .decoders[SAX_TYPE_UINT8] = false_decoder,
+      },
+      &err));
+  ASSERT_STR_EQ("Failed to set \"@content\" with \"42\"; decoder returned false", err);
   arena_custom_alloc(arena, err, 0);
 
   sax_parser_free(parser);
@@ -79,6 +109,7 @@ static void test_mapper_set_error(arena_t *restrict arena) {
 
 TEST(map_malformed) {
   test_mapper_no_decoder(arena);
-  test_mapper_decode_returns_false(arena);
+  test_mapper_decode_attr_returns_false(arena);
+  test_mapper_decode_content_returns_false(arena);
   test_mapper_set_error(arena);
 }
