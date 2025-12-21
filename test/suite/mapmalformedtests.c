05da5386cedf58d28893a10518c11dab06f8709a
Chad Hartman <cmhartman@gmail.com>
2025-12-20 16:15:17 -0800

    wip

---
+++ test/suite/mapmalformedtests.c
@@ -0,0 +1,19 @@
+#include <saxmapper.h>
+#include <test.h>
+
+TEST(map_malformed) {
+
+  sax_parser_t *restrict parser = sax_parser(&(sax_config_t){
+      .alloc = arena_custom_alloc,
+      .alloc_ctx = arena,
+      .xml = "<foo",
+  });
+
+  const sax_field_t schema = {0};
+  char *err = NULL;
+  char buf[128];
+  ASSERT_FALSE(sax_decode(parser, &schema, buf, NULL, &err));
+  ASSERT_STR_EQ("Unexpected termination at line 1 column 5", err);
+
+  sax_parser_free(parser);
+}
